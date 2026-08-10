a51bf7922fe5fc28ec48ed56ea60b704018be647
naoya.kohda <naoya.kohda@gmail.com>
2026-08-11 04:41:40 +0900

    Elixirのバージョンを1.14から1.18に更新

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExParamsSchema.MixProject do
       app: :ex_params_schema,
       version: "0.1.0",
       description: "LiveView イベントパラメーターを型付き構造体へ変換するライブラリ",
-      elixir: "~> 1.14",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       test_coverage: [summary: [threshold: 90]],
       package: package(),
